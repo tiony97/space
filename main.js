23bd360f4971ee17eae89ab1c06d6e68d41dee4d
Tiony <53773743+tiony97@users.noreply.github.com>
2022-05-13 22:40:55 +0300

    home page responsive

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Full Screen Menu
 $(document).ready(function() {
     $('.menuOpen').on('click', function() {
        $('.menuOverlay').addClass('open');
@@ -6,4 +7,24 @@ $(document).ready(function() {
     $('.menuClose').on('click', function() {
       $('.menuOverlay').removeClass('open');
     });
+});
+
+//Fix navigation to top of page
+var stickyOffset = $('nav').offset().top;
+var stickyOffset2 = $('.responsiveNav').offset().top;
+
+$(window).scroll(function() {
+
+    if ( $(window).scrollTop() > stickyOffset){
+        $('nav').addClass('fixed');
+    } else {
+        $('nav').removeClass('fixed');
+    } 
+
+    //Responsive Nav
+    if ( $(window).scrollTop() > stickyOffset2){
+        $('.responsiveNav').addClass('fixed');
+    } else {
+        $('.responsiveNav').removeClass('fixed');
+    }
 });
